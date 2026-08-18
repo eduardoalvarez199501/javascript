11a2cfd83aee3401abe2fb084d8bfc88532526bd
Eduardo Alvarez <eduardo.alvarez.199501@gmail.com>
2026-08-18 13:38:17 -0600

    hola seccion 1

---
+++ pf-js/app.js
@@ -0,0 +1,19 @@
+console.log("hola mundo")
+
+let nombre=1;
+let nombre_2 = "hola";
+const version_1 ="tralala";
+/**
+ * crear una variable y darle un valor se le llama asignacion 
+ * las variables pueden ser reasignadas 
+ * = / operador asignacion 
+ * para usar ls variables no deber ir en "" al llamarlas 
+ * palabras reservadas , usadas por el lenguaje de programacion y ya n se pueden asignar o utilizar en otro lado
+ * 
+ */
+
+console.log(nombre);
+console.log(nombre_2);
+//error
+//console.log(nombre_3);
+//version_1="oye mi amor";
